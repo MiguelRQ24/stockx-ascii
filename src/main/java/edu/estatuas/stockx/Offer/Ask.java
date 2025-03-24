3dd113f5b88e3f073ef722ee0b483d56540232a6
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2025-03-24 13:24:16 +0100

    fix(Ask): cambiar tipo de dato del ask a Integer

--- src/main/java/edu/estatuas/stockx/Offer/Ask.java
+++ src/main/java/edu/estatuas/stockx/Offer/Ask.java
@@ -2,7 +2,7 @@ package edu.estatuas.stockx.Offer;
 
 public class Ask implements Offer{
     private String size;
-    private int ask;
+    private Integer ask;
     public Ask(String size, int ask){
         this.size = size;
         this.ask = ask;
