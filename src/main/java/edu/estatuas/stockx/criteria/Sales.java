6c9a3d4bcff4ee07ca72b60a48d74da4432b6c48
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2025-03-24 13:26:36 +0100

    feat(Sales):Crear la clase Sales.java

---
+++ src/main/java/edu/estatuas/stockx/criteria/Sales.java
@@ -0,0 +1,24 @@
+package edu.estatuas.stockx.criteria;
+
+import edu.estatuas.stockx.Item.Item;
+import edu.estatuas.stockx.Offer.Offer;
+import edu.estatuas.stockx.Offer.Sale;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sales implements Criteria{
+
+
+    @Override
+    public List<Offer> checkCriteria(Item item){
+        List<Offer> offers = new ArrayList<>();
+        for (Offer offer: item.getOffers()){
+            if (offer.getClass() == Sale.class){
+                offers.add(offer);
+            }
+        }
+        return offers;
+    }
+
+}
