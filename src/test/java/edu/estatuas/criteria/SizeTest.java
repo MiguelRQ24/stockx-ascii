9a10b5139950f5f29b4b7f15cff9d0f87c9c8b62
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2025-03-26 11:12:58 +0100

    test(SizeTest):Añadir test SizeTest.java

---
+++ src/test/java/edu/estatuas/criteria/SizeTest.java
@@ -0,0 +1,43 @@
+package edu.estatuas.criteria;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+
+import edu.estatuas.stockx.criteria.*;
+import org.junit.jupiter.api.Test;
+
+import edu.estatuas.stockx.offer.Ask;
+import edu.estatuas.stockx.offer.Bid;
+import edu.estatuas.stockx.offer.Offer;
+import edu.estatuas.stockx.item.Sneaker;
+
+public class SizeTest {
+
+    @Test
+    public void checkCriteria_size_Test() {
+        Sneaker sneaker = new Sneaker("555088-105", "Jordan 1");
+        sneaker.add(new Bid("13", 550));
+        sneaker.add(new Bid("4.5", 480));
+        sneaker.add(new Bid("13", 900));
+        sneaker.add(new Bid("6", 472));
+        sneaker.add(new Ask("15", 288));
+        sneaker.add(new Ask("13", 333));
+        sneaker.add(new Ask("14", 1000));
+        sneaker.add(new Ask("13", 341));
+
+        Criteria size = new Size("13");
+        List<Offer> filteredSize = size.checkCriteria(sneaker);
+        assertTrue(filteredSize.stream().allMatch(o -> o.size().equals("13")));
+    }
+
+    @Test
+    public void checkCriteria_no_size_Test() {
+        Sneaker sneaker = new Sneaker("555088-105", "Jordan 1");
+        Criteria size = new Size("5");
+        List<Offer> filteredSize = size.checkCriteria(sneaker);
+        assertTrue(filteredSize.isEmpty());
+    }
+}
